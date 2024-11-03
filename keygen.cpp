25f31ff8c96da677550507f921b4ff70baa37ab3
idonwannaa <egor.nalivajko@yandex.ru>
2024-11-03 05:34:36 -0800

    added generation of byteVector

--- keygen.cpp
+++ keygen.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <bitset>
+#include <sstream>
 
 using namespace std;
 
@@ -18,10 +19,9 @@ int disriminant(int a, int b, int c) {
 
 uint8_t createByte(int &a, int &b, int &c) {
     string byte_string = "";
-    int d;
 
     for (int i = 0; i < 8; i++) {
-        d = disriminant(a, b, c);
+        int d = disriminant(a, b, c);
         if (d % 2 == 0) byte_string += "0"; else byte_string += "1";
         shiftOdds(a, b, c, d);
     }
@@ -31,6 +31,29 @@ uint8_t createByte(int &a, int &b, int &c) {
 }
 
 void generateKey(string file_name) {
+    int a, b, c, length;
+    vector<uint8_t> byteVector;
+
+    cout << endl << "Введите длину ключа в байтах: ";
+    cin >> length;
+    cout << "Введите коэффиценты квадратного уравнения ax^2 + bx + c = 0 через пробел: ";
+    cin >> a >> b >> c;
+    cout << endl;
+
+    for (int i = 0; i < length; i++) {
+        uint8_t byte = createByte(a, b, c);
+        byteVector.push_back(byte);
+    }
+
+    cout << "Сгенерированный ключ: ";
+    for (uint8_t byte: byteVector) {
+        stringstream ss;
+        ss << hex << setw(2) << setfill('0') << static_cast<int>(byte);
+        cout << ss.str() << " ";
+    }
+    
+    cout << endl << endl;
+    cout << "Ключ записан в файл " << file_name << endl << endl;
 }
 
 int main(int argc, char* argv[]) {
