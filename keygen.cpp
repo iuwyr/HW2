24404081120d6eaa3e063dc108e8449d4adaf945
idonwannaa <egor.nalivajko@yandex.ru>
2024-11-03 05:49:28 -0800

    added writeBytesToFile func

--- keygen.cpp
+++ keygen.cpp
@@ -30,9 +30,31 @@ uint8_t createByte(int &a, int &b, int &c) {
     return byte;
 }
 
+void writeBytesToFile(const vector<uint8_t> &byte_vector, const string &file_name) {
+    ofstream outFile(file_name, ios::binary);
+
+    if (file_name.substr(file_name.find_last_of('.')) == ".bin") {
+        outFile.write(reinterpret_cast<const char*> (byte_vector.data()), byte_vector.size());
+        cout << "Ключ записан в файл " << file_name << endl << endl;
+    } 
+    
+    else if (file_name.substr(file_name.find_last_of('.')) == ".txt") {
+        for (const uint8_t &byte : byte_vector) {
+            outFile << hex << setw(2) << setfill('0') << static_cast<int>(byte);
+        }
+        cout << "Ключ записан в файл " << file_name << endl << endl;
+    } 
+    
+    else {
+        cout << "Неподдерживаемое расширение файла: " << file_name << endl << endl;
+    }
+
+    outFile.close();
+}
+
 void generateKey(string file_name) {
     int a, b, c, length;
-    vector<uint8_t> byteVector;
+    vector<uint8_t> byte_vector;
 
     cout << endl << "Введите длину ключа в байтах: ";
     cin >> length;
@@ -42,18 +64,18 @@ void generateKey(string file_name) {
 
     for (int i = 0; i < length; i++) {
         uint8_t byte = createByte(a, b, c);
-        byteVector.push_back(byte);
+        byte_vector.push_back(byte);
     }
 
     cout << "Сгенерированный ключ: ";
-    for (uint8_t byte: byteVector) {
+    for (uint8_t byte: byte_vector) {
         stringstream ss;
         ss << hex << setw(2) << setfill('0') << static_cast<int>(byte);
         cout << ss.str() << " ";
     }
-    
     cout << endl << endl;
-    cout << "Ключ записан в файл " << file_name << endl << endl;
+
+    writeBytesToFile(byte_vector, file_name);
 }
 
 int main(int argc, char* argv[]) {
